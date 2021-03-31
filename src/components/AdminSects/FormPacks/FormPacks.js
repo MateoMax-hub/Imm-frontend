3a3b05f60892a7f7fbcc8c05ba24940392a3536e
andrescuello7 <andrescuellotrabajo@gmail.com>
2021-03-31 18:14:34 -0300

    actualizar

--- src/components/AdminSects/FormPacks/FormPacks.js
+++ src/components/AdminSects/FormPacks/FormPacks.js
@@ -5,50 +5,61 @@ function FormPacks({ token }) {
     const { handleClose, handleShow, HandleSubmit, HandleChange, show, CardPerfil } = UsePacks({ token });
     return (
         <div>
-            <div>
-                <Button variant="primary" onClick={handleShow}>
-                    + Agregar Pack
-                </Button>
-
-                <Modal show={show} onHide={handleClose}>
-                    <div className="p-2 m-2">
-                        <Form onSubmit={HandleSubmit}>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>Titulo</Form.Label>
-                                <Form.Control onChange={(e) => HandleChange(e)} name="titulo" type="text" />
-                            </Form.Group>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>Imagen</Form.Label>
-                                <div>
-                                    <Form.Control
-                                        type="file"
-                                        className="btn btn-secondary"
-                                    />
-                                </div>
-                            </Form.Group>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>Descripcion</Form.Label>
-                                <div>
+            <div className="w-100 d-flex justify-content-center">
+                <div>
+                    <Modal show={show} onHide={handleClose}>
+                        <div className="p-2 m-2">
+                            <Form onSubmit={HandleSubmit}>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Titulo</Form.Label>
                                     <Form.Control
                                         onChange={(e) => HandleChange(e)}
-                                        name="descripcion"
+                                        name="titulo"
                                         type="text"
                                     />
-                                </div>
-                            </Form.Group>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>Precio</Form.Label>
-                                <Form.Control onChange={(e) => HandleChange(e)} name="precio" type="number" />
-                            </Form.Group>
-                            <Button variant="primary" type="submit">
-                                Subir Pack
-                            </Button>
-                        </Form>
+                                </Form.Group>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Imagen</Form.Label>
+                                    <div>
+                                        <Form.Control type="file" />
+                                    </div>
+                                </Form.Group>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Descripcion</Form.Label>
+                                    <div>
+                                        <Form.Control
+                                            onChange={(e) => HandleChange(e)}
+                                            name="descripcion"
+                                            type="text"
+                                        />
+                                    </div>
+                                </Form.Group>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>Precio</Form.Label>
+                                    <Form.Control
+                                        onChange={(e) => HandleChange(e)}
+                                        name="precio"
+                                        type="number"
+                                    />
+                                </Form.Group>
+                                <Button variant="primary" className="w-100" type="submit">
+                                    Subir Pack
+                                </Button>
+                            </Form>
+                        </div>
+                    </Modal>
+                </div>
+
+                <div className="cardPacks">
+                    <div className="agregarProducto" onClick={handleShow}>
+                        <a>
+                            <b>
+                                <i>+ Agregar Pack</i>
+                            </b>
+                        </a>
                     </div>
-                </Modal>
-            </div>
-            <div className="d-flex justify-content-center flex-wrap">
-                <>{CardPerfil}</>
+                    <>{CardPerfil}</>
+                </div>
             </div>
         </div>
     );
